b3002f0561eef5e09d9c48b1a5b904123512f673
kaptaincleland <kaptaincleland@gmail.com>
2024-06-16 02:56:08 +0000

    16/06/2024 2:56 AMS

--- Grading-system/grading-system.cpp
+++ Grading-system/grading-system.cpp
@@ -1,5 +1,3 @@
-#include<iostream>
-using namespace std;
 /*write a program that would accept the mark of a user once accepted grade as follow
 A = 100-70
 B = 69-60
@@ -7,14 +5,20 @@ C = 59-50
 D = 49-40
 E = 39-30
 F = 29-0*/
+/*NESTA CLELAND OKINE
+  (ID)170056217
+  BIT GROUP C*/
+#include <iostream>
+using namespace std;
+
 int main () {
     int mark;
     char grade;
 
     cout << "Enter your mark: ";
-    cin >> mark; 
+    cin >> mark;
 
-    if (mark >= 70){
+    if (mark >= 70) {
         grade = 'A';
     } else if (mark >= 60) {
         grade = 'B';
@@ -27,7 +31,7 @@ int main () {
     } else {
         grade = 'F';
     }
-    
-    cout << "Your Grade is: " << grade << endl;
+
+    cout << "Your Grade for the mark "<< mark << " is "<< grade << endl;
     return 0;
 }
